ff0a45e9ad6c36f6628654d1c68d71f430310ef7
surimanw <surimanw@gmail.com>
2022-06-21 03:32:43 -0300

    server.jspublic

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
     <footer class="bg-gray-200 text-center lg:text-left">
       <div class="text-gray-700 text-center p-4 bg-blue-600">
         <BotComponent />
-        © 2022 Copyright: ->
+        © 2022 Copyright:
         <a class="text-gray-800" href="https://tailwind-elements.com/">Francisco Grillo</a>
       </div>
     </footer>
--- src/main.js
+++ src/main.js
@@ -3,23 +3,20 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import './index.css';
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-Vue.use(VueAxios, axios)
-import VueNoty from 'vuejs-noty'
-Vue.use(VueNoty)
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+Vue.use(VueAxios, axios);
+import VueNoty from 'vuejs-noty';
+Vue.use(VueNoty);
 import VueMeta from 'vue-meta';
 Vue.use(VueMeta);
 // import VeeValidate from 'vee-validate';
 // Vue.use(VeeValidate);
-import Ads from 'vue-google-adsense'
-
-Vue.use(require('vue-script2'))
-
-Vue.use(Ads.Adsense)
-Vue.use(Ads.InArticleAdsense)
-Vue.use(Ads.InFeedAdsense)
-
+import Ads from 'vue-google-adsense';
+Vue.use(require('vue-script2'));
+Vue.use(Ads.Adsense);
+Vue.use(Ads.InArticleAdsense);
+Vue.use(Ads.InFeedAdsense);
 
 
 Vue.config.productionTip = false;
